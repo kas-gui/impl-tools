2951dc35c649215090802f7e6a0dc7280993b78a
Diggory Hardy <git@dhardy.name>
2022-11-16 16:34:07 +0000

    tests/newtype: expand to dyn, Rc, Arc

--- tests/newtype.rs
+++ tests/newtype.rs
@@ -1,6 +1,8 @@
 //! Test implementing traits over newtype wrappers
 
 use impl_tools::autoimpl;
+use std::rc::Rc;
+use std::sync::Arc;
 
 #[autoimpl(for<T: trait> &T, &mut T, Box<T>)]
 trait Foo {
@@ -14,23 +16,63 @@ impl Foo for S {
     }
 }
 
-#[autoimpl(Deref, DerefMut using self.0)]
-struct NewFoo<T: Foo>(T);
-
-#[autoimpl(Deref, DerefMut using self.0)]
-struct FooRef<'a>(&'a dyn Foo);
-
-#[autoimpl(Deref, DerefMut using self.0)]
-struct FooRefMut<'a>(&'a mut dyn Foo);
-
-#[autoimpl(Deref, DerefMut using self.0)]
-struct BoxFoo<T: Foo>(Box<T>);
+#[test]
+fn direct() {
+    assert!(S.success());
+}
 
 #[test]
-fn foo() {
-    assert!(S.success());
-    assert!(NewFoo(S).success());
-    assert!(FooRef(&S).success());
-    assert!(FooRefMut(&mut S).success());
-    assert!(BoxFoo(Box::new(S)).success());
+fn new_foo() {
+    #[autoimpl(Deref, DerefMut using self.0)]
+    struct NewType<T: Foo>(T);
+
+    assert!(NewType(S).success());
+}
+
+#[test]
+fn new_foo_ref() {
+    #[autoimpl(Deref, DerefMut using self.0)]
+    struct NewType<'a>(&'a dyn Foo);
+
+    assert!(NewType(&S).success());
+}
+
+#[test]
+fn new_foo_ref_mut() {
+    #[autoimpl(Deref, DerefMut using self.0)]
+    struct NewType<'a>(&'a mut dyn Foo);
+
+    assert!(NewType(&mut S).success());
+}
+
+#[test]
+fn new_foo_box() {
+    #[autoimpl(Deref, DerefMut using self.0)]
+    struct NewType<T: Foo>(Box<T>);
+
+    assert!(NewType(Box::new(S)).success());
+}
+
+#[test]
+fn new_foo_box_dyn() {
+    #[autoimpl(Deref, DerefMut using self.0)]
+    struct NewType(Box<dyn Foo>);
+
+    assert!(NewType(Box::new(S)).success());
+}
+
+#[test]
+fn new_foo_rc_dyn() {
+    #[autoimpl(Deref, DerefMut using self.0)]
+    struct NewType(Rc<dyn Foo>);
+
+    assert!(NewType(Rc::new(S)).success());
+}
+
+#[test]
+fn new_foo_arc_dyn() {
+    #[autoimpl(Deref, DerefMut using self.0)]
+    struct NewType(Arc<dyn Foo + Send + Sync>);
+
+    assert!(NewType(Arc::new(S)).success());
 }
